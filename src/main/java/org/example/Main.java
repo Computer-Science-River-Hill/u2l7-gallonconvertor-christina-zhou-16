96726bbde9da1b2ce6d8725251b33259120c712d
christina-zhou-16 <117242473+christina-zhou-16@users.noreply.github.com>
2025-02-25 15:06:59 +0000

    fewa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,29 @@
 package org.example; //DO NOT DELETE
+import java.util.Scanner;
+/*
+ * Program Name: Gallon Converter
+ * Author: Christina Zhou
+ */
+class Main {
+    public static void main(String[] args){
+        //Start code here
+        double gallons;
+        double quarts;
+        double pints;
+        double cups;
+        double tablespoons;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of gallons: ");
+        gallons = input.nextDouble();
+        input.close();
+        System.out.println("In " + gallons + " gallons there are: ");
+        quarts = gallons * 4;
+        pints = quarts * 2;
+        cups = pints * 2;
+        tablespoons = cups * 16;
+        System.out.println(quarts + " quarts");
+        System.out.println(pints + " pints");
+        System.out.println(cups + " cups");
+        System.out.println(tablespoons + " tablespoons");
+    }
+}
